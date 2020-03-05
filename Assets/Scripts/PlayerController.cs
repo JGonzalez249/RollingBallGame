4a96dfcff129e7ae13277b072e02fed3623d4405
Adam Brewer <ahcbrewer@knights.ucf.edu>
2020-03-04 19:22:49 -0500

    "Ghosting"
    Co routine made

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	
+	Collider m_Collider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Collider = GetComponent<Collider>();
     }
+
+
+IEnumerator Ghosting(){
+if (m_Collider.enabled == true){
+		m_Collider.enabled = !m_Collider.enabled;
+		yield return new WaitForSeconds(1);
+		m_Collider.enabled = !m_Collider.enabled;
+        }
+}
+
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
-		
-        }
+	StartCoroutine(Ghosting());
+	}
+
     }
 }

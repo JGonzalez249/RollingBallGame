ea11911387d801584b25c019dc88dfbf6351604b
Jonathan Gonzalez <jgonzalez249@gmail.com>
2020-03-17 14:51:12 -0400

    Pushing revert

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,35 +1,32 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-	
-	Collider m_Collider;
+    private Collider m_Collider;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         m_Collider = GetComponent<Collider>();
     }
 
-
-IEnumerator Ghosting(){
-if (m_Collider.enabled == true){
-		m_Collider.enabled = !m_Collider.enabled;
-		yield return new WaitForSeconds(1);
-		m_Collider.enabled = !m_Collider.enabled;
+    private IEnumerator Ghosting()
+    {
+        if (m_Collider.enabled == true)
+        {
+            m_Collider.enabled = !m_Collider.enabled;
+            yield return new WaitForSeconds(1);
+            m_Collider.enabled = !m_Collider.enabled;
         }
-}
-
+    }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
-	StartCoroutine(Ghosting());
-	}
-
+            StartCoroutine(Ghosting());
+        }
     }
 }

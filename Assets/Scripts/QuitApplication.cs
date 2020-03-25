5521134292b3f1d14ede16a9a3a8e9422dffc875
Ryan <rnf.fisher@gmail.com>
2020-03-24 20:33:10 -0400

    Quit application

---
+++ Assets/Scripts/QuitApplication.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+
+public class QuitApplication : MonoBehaviour
+{
+    private bool beGone;
+
+    private void Update()
+    {
+        if (beGone == true)
+        {
+            Application.Quit();
+            Debug.Log("You have been Exiled! Be Gone! (Quit)");
+        }
+    }
+
+    private IEnumerator OnQuit(InputValue value)
+    {
+        yield return beGone = true;
+    }
+}

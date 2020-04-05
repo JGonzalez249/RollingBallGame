9b2d51f29c43f7f27ad2a37b3f0b153b4caebc61
Adam Brewer <ahcbrewer@knights.ucf.edu>
2020-04-04 22:10:41 -0400

    Artcam stuff
    Probably doesn't work but doesn't really matter so may as well

---
+++ Assets/Scripts/ArtCam.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// A simple free camera to be added to a Unity game object.
+/// 
+/// Keys:
+///	wasd / arrows	- movement
+///	q/e 			- up/down (local space)
+///	r/f 			- up/down (world space)
+///	pageup/pagedown	- up/down (world space)
+///	hold shift		- enable fast movement mode
+///	right mouse  	- enable free look
+///	mouse			- free look / rotation
+///     
+/// </summary>
+
+public class ArtCam : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public float fastMovementSpeed = 100f;
+    public float freeLookSensitivity = 3f;
+    public float zoomSensitivity = 10f;
+    public float fastZoomSensitivity = 50f;
+    private bool looking = false;
+
+    private IEnumerator OnLeft(InputValue value)
+    {
+        yield return transform.position = transform.position + (-transform.right * movementSpeed * Time.deltaTime);
+    }
+
+    private IEnumerator OnRight(InputValue value)
+        {
+        yield return transform.position = transform.position + (transform.right * movementSpeed * Time.deltaTime);
+        }
+
+    private IEnumerator OnUp(InputValue value)
+        {
+        yield return transform.position = transform.position + (transform.forward * movementSpeed * Time.deltaTime);
+        }
+
+    private IEnumerator OnDown(InputValue value)
+        {
+        yield return transform.position = transform.position + (-transform.forward * movementSpeed * Time.deltaTime);
+        }
+
+    void Update()
+    {
+        var fastMode = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        var movementSpeed = fastMode ? this.fastMovementSpeed : this.movementSpeed;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.position = transform.position + (transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.position = transform.position + (-transform.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.R) || Input.GetKey(KeyCode.PageUp))
+        {
+            transform.position = transform.position + (Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.F) || Input.GetKey(KeyCode.PageDown))
+        {
+            transform.position = transform.position + (-Vector3.up * movementSpeed * Time.deltaTime);
+        }
+
+        if (looking)
+        {
+            float newRotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * freeLookSensitivity;
+            float newRotationY = transform.localEulerAngles.x - Input.GetAxis("Mouse Y") * freeLookSensitivity;
+            transform.localEulerAngles = new Vector3(newRotationY, newRotationX, 0f);
+        }
+
+        float axis = Input.GetAxis("Mouse ScrollWheel");
+        if (axis != 0)
+        {
+            var zoomSensitivity = fastMode ? this.fastZoomSensitivity : this.zoomSensitivity;
+            transform.position = transform.position + transform.forward * axis * zoomSensitivity;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            StartLooking();
+        }
+        else if (Input.GetKeyUp(KeyCode.Mouse1))
+        {
+            StopLooking();
+        }
+    }
+
+    void OnDisable()
+    {
+        StopLooking();
+    }
+    public void StartLooking()
+    {
+        looking = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public void StopLooking()
+    {
+        looking = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+}

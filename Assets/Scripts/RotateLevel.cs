cd519f2490f7fe13277e33ef323ac2d844d2590a
Ryan <rnf.fisher@gmail.com>
2020-04-15 08:33:01 -0400

    updated level
    added front walls that disappear when turning

--- Assets/Scripts/RotateLevel.cs
+++ Assets/Scripts/RotateLevel.cs
@@ -7,6 +7,8 @@ public class RotateLevel : MonoBehaviour
     public Rigidbody prb;
     public GameObject lvl;
     public Vector3 rotatePoint;
+    private GameObject[] Wall1;
+    private GameObject[] Wall2;
 
     private bool isRotatingRight;
     private bool isRotatingLeft;
@@ -20,30 +22,55 @@ public class RotateLevel : MonoBehaviour
         isRotatingLeft = false;
         prb = GameObject.Find("Player").GetComponent<Rigidbody>();
         lvl = GameObject.FindGameObjectWithTag("Level");
+        {
+            print("Cannot Find Level");
+        }
+
+        Wall1 = GameObject.FindGameObjectsWithTag("Wall1");//what is ghostable wall?
+        if (GameObject.FindGameObjectsWithTag("Wall1") == null) // if cannot find any ghostable walls in level
+        {
+            print("Cannot Find Wall1");
+        }
+
+        Wall2 = GameObject.FindGameObjectsWithTag("Wall2");//what is ghostable wall?
+        if (GameObject.FindGameObjectsWithTag("Wall2") == null) // if cannot find any ghostable walls in level
+        {
+            print("Cannot Find Wall2");
+        }
+
+        foreach (GameObject go in Wall1) // for every front-rotated front wall
+        {
+            go.SetActive(false);
+        }
+        foreach (GameObject go in Wall2) // for every side-rotated front wall
+        {
+            go.SetActive(true);
+        }
     }
     private void Update()
     {
+
         if (isRotatingLeft == false && isRotatingRight == false)
         {
             lvl.transform.rotation = Quaternion.Euler(0, currentYRot, 0);
         }
         if (isRotatingRight == true)
         {
-            this.transform.position = new Vector3(rotatePoint.x, this.transform.position.y, rotatePoint.z);
+            this.transform.position = new Vector3(rotatePoint.x, this.transform.position.y, 0);
             lvl.transform.RotateAround(prb.transform.position, Vector3.up, 172.5f * Time.deltaTime); // 90 degrees
             currentYRot = lvl.transform.rotation.eulerAngles.y;
-            
+
         }
         else if (isRotatingLeft == true)
         {
-            this.transform.position = new Vector3(Mathf.Round(rotatePoint.x), this.transform.position.y, rotatePoint.z);
+            this.transform.position = new Vector3(Mathf.Round(rotatePoint.x), this.transform.position.y, 0);
             lvl.transform.RotateAround(prb.transform.position, Vector3.down, 172.5f * Time.deltaTime);// 90 degrees
             currentYRot = lvl.transform.rotation.eulerAngles.y;
-            
+
         }
 
         //rounding
-        if (currentYRot > -5 && currentYRot < 5)
+        if (currentYRot > -10 && currentYRot < 10)
         {
             currentYRot = 0;
         }
@@ -65,6 +92,29 @@ public class RotateLevel : MonoBehaviour
             lvl.transform.rotation = Quaternion.Euler(0, currentYRot, 90);
         }
 
+        //walls
+        if (currentYRot > -45 && currentYRot < 45 || currentYRot > 135 && currentYRot < 225 || currentYRot < -135 && currentYRot > -225)
+        {
+            foreach (GameObject go in Wall1) // for every front-rotated front wall
+            {
+                go.SetActive(false);
+            }
+            foreach (GameObject go in Wall2) // for every side-rotated front wall
+            {
+                go.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (GameObject go in Wall1) // for every front-rotated front wall
+            {
+                go.SetActive(true);
+            }
+            foreach (GameObject go in Wall2) // for every side-rotated front wall
+            {
+                go.SetActive(false);
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -88,7 +138,7 @@ public class RotateLevel : MonoBehaviour
         isRotatingRight = true;
         yield return new WaitForSeconds(0.5f);
         isRotatingRight = false;
-        prb.AddForce(5, 0, 0, ForceMode.Impulse);
+        prb.AddForce(10, 0, 0, ForceMode.Impulse);
         cooldown = true;
         yield return new WaitForSeconds(0.1f);
         cooldown = false;

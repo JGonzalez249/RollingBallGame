acc3cad36aa7cf751bd30b4352337bb40c4c89df
Ryan <rnf.fisher@gmail.com>
2020-04-25 19:40:16 -0400

    Merge fix

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -12,18 +12,12 @@ public class Respawn : MonoBehaviour
 
     public bool death;
 
-    private float lvlRotationReset;
-    private Vector3 lvlPosReset;
-
     // Start is called before the first frame update
     private void Start()
     {
         death = false;
         RespawnTxt.text = "";
         RespawnPosition = GameObject.FindGameObjectWithTag("Respawn");
-
-        lvlPosReset = GameObject.FindGameObjectWithTag("Player").GetComponent<RotateLevel>().lvl.transform.position;
-        lvlRotationReset = GameObject.FindGameObjectWithTag("Player").GetComponent<RotateLevel>().currentYRot;
     }
 
     // Update is called once per frame
@@ -45,16 +39,9 @@ public class Respawn : MonoBehaviour
 
     private IEnumerator respawn()
     {
-        //rotation
-        GameObject.FindGameObjectWithTag("Player").GetComponent<RotateLevel>().currentYRot = lvlRotationReset;
-        GameObject.FindGameObjectWithTag("Player").GetComponent<RotateLevel>().lvl.transform.position = lvlPosReset;
-
         death = false;
 
         //move to respawn position
-        yield return new WaitForSeconds(0.1f);
-        this.transform.position = new Vector3(RespawnPosition.transform.position.x, RespawnPosition.transform.position.y, RespawnPosition.transform.position.z);
-
         RespawnTxt.text = "Respawn";
         GameObject.Find("Player").GetComponent<Ghosting>().usingGhosting = false; // turn off ghosting
         GameObject.Find("Player").GetComponent<Movement>().force = 0f; // reset force
@@ -67,8 +54,6 @@ public class Respawn : MonoBehaviour
 
     private IEnumerator checkpoint()
     {
-        lvlRotationReset = GameObject.FindGameObjectWithTag("Player").GetComponent<RotateLevel>().currentYRot;
-        lvlPosReset = GameObject.FindGameObjectWithTag("Player").GetComponent<RotateLevel>().lvl.transform.position;
         RespawnTxt.text = "Checkpoint";
         RespawnPosition = GameObject.FindGameObjectWithTag("Checkpoint");
         yield return new WaitForSeconds(1f);
